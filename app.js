4628ee42aa00421e4718ce36b4d4f1dcf7f4da39
Alvin Galit <aroblesgalit@gmail.com>
2020-04-14 15:37:46 -0500

    Created team html file using writefile method

--- app.js
+++ app.js
@@ -29,15 +29,19 @@ async function init() {
         // After the user has input all employees desired, call the `render` function (required
         // above) and pass in an array containing all employee objects; the `render` function will
         // generate and return a block of HTML including templated divs for each employee!
-        const mainHtml = await render(employees);
-        console.log(mainHtml);
+        const team = await render(employees);
 
         // After you have your html, you're now ready to create an HTML file using the HTML
         // returned from the `render` function. Now write it to a file named `team.html` in the
         // `output` folder. You can use the variable `outputPath` above target this location.
         // Hint: you may need to check if the `output` folder exists and create it if it
         // does not.
-        // fs.writeFile(outputPath, mainHtml);
+        fs.writeFile(outputPath, team, function(err) {
+            if (err) {
+                console.log(err);
+            }
+            console.log("team.html has been created")
+        });
 
     } catch (err) {
         console.log(err);
