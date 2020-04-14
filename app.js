50776719c45ee8086a7b034c30d0088bce2dd8ae
Alvin Galit <aroblesgalit@gmail.com>
2020-04-14 16:30:51 -0500

    Added join method when pushing the engineers and interns into the html array

--- app.js
+++ app.js
@@ -30,6 +30,7 @@ async function init() {
         // above) and pass in an array containing all employee objects; the `render` function will
         // generate and return a block of HTML including templated divs for each employee!
         const team = await render(employees);
+        console.log(team);
 
         // After you have your html, you're now ready to create an HTML file using the HTML
         // returned from the `render` function. Now write it to a file named `team.html` in the
